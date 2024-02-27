565ae96f21998689cb00d9eff56c704f147d79ba
kasgimhana <kaasgimhana@gmail.com>
2024-02-28 00:28:34 +0530

    signin, login pages

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
